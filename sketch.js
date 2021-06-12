bd4c8dde145e0984aef7d873f62850a688e6f6cb
Binaya6335 <85494971+Binaya6335@users.noreply.github.com>
2021-06-12 20:43:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var b = 0;
 
 function setup() {
   createCanvas(400, 400);  
-}
+
 
 btn_red=createButton("RED");
 btn_red.position(100,50);
@@ -16,7 +16,7 @@ btn_red.mousePressed(red_bg);
 btn_green=createButton("GREEN");
 btn_green.position(100,50);
 btn_green.mousePressed(green_bg);
-
+}
 function draw() {
   background(r,g,b);
 }
